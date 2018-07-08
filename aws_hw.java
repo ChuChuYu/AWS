3be04679882cdddfc730d1f1133d904709f4b0b6
ChuChuYu <40236253+ChuChuYu@users.noreply.github.com>
2018-07-08 19:59:13 +0800

    Add files via upload

---
+++ aws_hw.java
@@ -0,0 +1,40 @@
+public class Human {
+
+    private String name;
+    private String job();
+
+    private class Hunter {
+        public final static int FIRE_BOW = 1;{
+            return int FIRE_ARROW = 1;
+        }
+        public final static int ICE_BOW = 2;{
+            return int ICE_ARROE = 2;
+        }
+    }   
+    
+    private class Warrior {
+        public final static int FIRE_BLADE = 1;{
+            return int FIRE_SLASH = 1;
+        }
+        public final static int ICE_BLADE = 2;{
+            return int ICE_SLASH = 2;       
+        }
+    }
+    
+    private class Mage {
+        public final static int FIRE_STAFF = 1;{
+            return int FIRE_BALL = 1;
+        }
+        public final static int ICE_STAFF = 2;
+            return int FROSTBOLT = 2;
+    }
+
+
+    public Human(){
+
+    }
+
+    public void attack(){
+        system.out.println("Fist Attack!")
+    }
+}
